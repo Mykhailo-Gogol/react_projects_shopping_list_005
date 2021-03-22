783786736d7430dad030ad2fd650e9ff66683186
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-03-22 16:33:59 +0200

    Додає проект

--- src/App.js
+++ src/App.js
@@ -1,50 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import "./index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft,
+  faCircle,
+  faCheckCircle,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
-	// HINT: each "item" in our list names a name, a boolean to tell if its been completed, and a quantity
-	const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [totalItemCount, setTotalItemCount] = useState(0);
 
-	return (
-		<div className='app-background'>
-			<div className='main-container'>
-				<div className='add-item-box'>
-					<input className='add-item-input' placeholder='Add an item...' />
-					<FontAwesomeIcon icon={faPlus} />
-				</div>
-				<div className='item-list'>
-					<div className='item-container'>
-						<div className='item-name'>
-							{/* HINT: replace false with a boolean indicating the item has been completed or not */}
-							{false ? (
-								<>
-									<FontAwesomeIcon icon={faCheckCircle} />
-									<span className='completed'>Item 1</span>
-								</>
-							) : (
-								<>
-									<FontAwesomeIcon icon={faCircle} />
-									<span>Item 1</span>
-								</>
-							)}
-						</div>
-						<div className='quantity'>
-							<button>
-								<FontAwesomeIcon icon={faChevronLeft} />
-							</button>
-							<span> 1 </span>
-							<button>
-								<FontAwesomeIcon icon={faChevronRight} />
-							</button>
-						</div>
-					</div>
-				</div>
-				<div className='total'>Total: 6</div>
-			</div>
-		</div>
-	);
+  const handleAddButtonClick = () => {
+    const newItem = {
+      itemName: inputValue,
+      quantity: 1,
+      isSelected: false,
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setInputValue("");
+    calculateTotal();
+  };
+  const toggleComplete = (index) => {
+    const newItems = [...items];
+    newItems[index].isSelected = !newItems[index].isSelected;
+    setItems(newItems);
+  };
+  const handleQuantityIncrease = (index) => {
+    const newItems = [...items];
+    newItems[index].quantity++;
+    setItems(newItems);
+    calculateTotal();
+  };
+  const handleQuantityDecrease = (index) => {
+    const newItems = [...items];
+    newItems[index].quantity--;
+    setItems(newItems);
+    calculateTotal();
+  };
+  const calculateTotal = () => {
+    const totalItemCount = items.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
+    setTotalItemCount(totalItemCount);
+  };
+
+  return (
+    <div className="app-background">
+      <div className="main-container">
+        <div className="add-item-box">
+          <input
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            className="add-item-input"
+            placeholder="Add an item..."
+          />
+          <FontAwesomeIcon
+            icon={faPlus}
+            onClick={() => handleAddButtonClick()}
+          />
+        </div>
+        <div className="item-list">
+          {items.map((item, index) => (
+            <div className="item-container">
+              <div
+                className="item-name"
+                onClick={() => {
+                  toggleComplete(index);
+                }}
+              >
+                {item.isSelected ? (
+                  <>
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                    <span className="completed">{item.itemName}</span>
+                  </>
+                ) : (
+                  <>
+                    <FontAwesomeIcon icon={faCircle} />
+                    <span>{item.itemName}</span>
+                  </>
+                )}
+              </div>
+              <div className="quantity">
+                <button>
+                  <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    onClick={() => handleQuantityDecrease(index)}
+                  />
+                </button>
+                <span> {item.quantity} </span>
+                <button>
+                  <FontAwesomeIcon
+                    icon={faChevronRight}
+                    onClick={() => handleQuantityIncrease(index)}
+                  />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="total">Total: {totalItemCount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
